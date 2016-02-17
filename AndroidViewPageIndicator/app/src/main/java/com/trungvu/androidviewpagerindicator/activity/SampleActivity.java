1a407de7fbb9a62ce937eab0b4a58ff672aba9f6
Trung Vu <trungvt89.it@gmail.com>
2016-02-18 01:49:44 +0900

    Init project & add ViewPagerIndicator source code

---
+++ AndroidViewPageIndicator/app/src/main/java/com/trungvu/androidviewpagerindicator/activity/SampleActivity.java
@@ -0,0 +1,18 @@
+package com.trungvu.androidviewpagerindicator.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.trungvu.androidviewpagerindicator.R;
+
+/**
+ * Created by TrungVT on 2/18/16.
+ */
+public class SampleActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedBundleInstance) {
+        super.onCreate(savedBundleInstance);
+        setContentView(R.layout.sample_activity);
+    }
+}
